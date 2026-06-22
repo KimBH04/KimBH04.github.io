23661cfff9627a390708ec947fa7ef186aac7d7b
KimBH <kimbh0437@gmail.com>
2026-06-22 22:31:53 +0900

    initinitinit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
+using Blog;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using source;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
